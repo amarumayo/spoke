8af1a7135af5d8a7f553ea221f6a292ba2f36c88
amarumayo <amarumayo@gmail.com>
2025-01-25 20:39:49 -0700

    added return gui window

--- spoke/__main__.py
+++ spoke/__main__.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import PySimpleGUI as sg
 from spoke import calculator
 
 def exists(var):
@@ -10,23 +11,25 @@ def exists(var):
         return False  # Variable doesn't exist
 
 
+# read the arguments passed in from command line
+this_client = "CLI"
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(
-        description="test"
+        description = "argument client used to toggle CLI or GUI"
     )
 
     parser.add_argument("--client", required = True)
     args = parser.parse_args()
     this_client = args.client 
 
-if not exists(this_client):
-    this_client = "CLI"
-
 valid_clients = {'GUI', 'CLI'}
 if not this_client in valid_clients:
     this_client = "CLI"
 
+
 def clear_screen():
+    """ function to clear the screen """
     # For Windows
     if os.name == 'nt':
         _ = os.system('cls')
@@ -37,4 +40,13 @@ def clear_screen():
 calculator = calculator.Calculator(client = this_client)
 ans = calculator.run()
 string = f"Right Spoke Length: {ans.get('right')} \nLeft Spoke Length: {ans.get('left')}"
-print(string)
+
+if this_client == "CLI":
+    print(string)
+
+elif this_client == "GUI":
+    sg.popup(string, title = 'Spoke Lengths')
+
+
+
+
---
+++ spoke/test.py
@@ -0,0 +1,5 @@
+import PySimpleGUI as sg
+import PySimpleGUI as sg
+
+x = "Hello, World!"
+sg.popup(x, title = "Spoke Lenghts")
